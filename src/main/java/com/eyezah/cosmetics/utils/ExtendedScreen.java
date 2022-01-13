ec95ce781c3ebbe268cf4fd066ccc8d58f2f7efc
valoeghese <valoeghesemods@gmail.com>
2022-01-13 14:30:40 +1300

    remove unused method

--- src/main/java/com/eyezah/cosmetics/utils/ExtendedScreen.java
+++ src/main/java/com/eyezah/cosmetics/utils/ExtendedScreen.java
@@ -1,10 +1,7 @@
 package com.eyezah.cosmetics.utils;
 
-import net.minecraft.client.gui.components.Widget;
 import net.minecraft.client.gui.components.events.GuiEventListener;
-import net.minecraft.network.chat.Component;
 
 public interface ExtendedScreen {
-	void setTitle(Component title);
 	Iterable<GuiEventListener> getChildren();
 }
