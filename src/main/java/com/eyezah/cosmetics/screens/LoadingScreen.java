0947421b366e9cfa41813ebe8fb8f7176c7768c1
Valoeghese <valoeghesemods@gmail.com>
2022-02-17 23:40:26 +1300

    unauthenticated screen shouldn't invade your game anymore

--- src/main/java/com/eyezah/cosmetics/screens/LoadingScreen.java
+++ src/main/java/com/eyezah/cosmetics/screens/LoadingScreen.java
@@ -42,28 +42,19 @@ public class LoadingScreen extends Screen implements LoadingTypeScreen {
 	@Override
 	protected void init() {
 		this.message = MultiLineLabel.create(this.font, this.reason, this.width - 50);
-		int var10001 = this.message.getLineCount();
-		Objects.requireNonNull(this.font);
-		this.textHeight = var10001 * 9;
-		int var10003 = this.width / 2 - 100;
-		int var10004 = this.height / 2 + this.textHeight / 2;
-		Objects.requireNonNull(this.font);
-		this.addRenderableWidget(new Button(var10003, Math.min(var10004 + 9, this.height - 30), 200, 20, new TranslatableComponent("options.skinCustomisation"), (button) -> {
+		this.textHeight = this.message.getLineCount() * 9;
+
+		this.addRenderableWidget(new Button(this.width / 2 - 100, Math.min(this.height / 2 + this.textHeight / 2 + 9, this.height - 30), 200, 20, new TranslatableComponent("options.skinCustomisation"), (button) -> {
 			this.minecraft.setScreen(new SkinCustomizationScreen(this.parentScreen, this.parentOptions));
 		}));
-		this.addRenderableWidget(new Button(var10003, Math.min(var10004 + 9, this.height - 30) + 24, 200, 20, new TranslatableComponent("gui.cancel"), (button) -> {
+		this.addRenderableWidget(new Button(this.width / 2 - 100, Math.min(this.height / 2 + this.textHeight / 2 + 9, this.height - 30) + 24, 200, 20, new TranslatableComponent("gui.cancel"), (button) -> {
 			this.minecraft.setScreen(this.parentScreen);
 		}));
 	}
 
 	public void render(PoseStack poseStack, int i, int j, float f) {
 		this.renderBackground(poseStack);
-		Font var10001 = this.font;
-		Component var10002 = this.title;
-		int var10003 = this.width / 2;
-		int var10004 = this.height / 2 - this.textHeight / 2;
-		Objects.requireNonNull(this.font);
-		drawCenteredString(poseStack, var10001, var10002, var10003, var10004 - 9 * 2, 11184810);
+		drawCenteredString(poseStack, this.font, this.title, this.width / 2, this.height / 2 - this.textHeight / 2 - 9 * 2, 11184810);
 		this.message.renderCentered(poseStack, this.width / 2, this.height / 2 - this.textHeight / 2);
 		super.render(poseStack, i, j, f);
 	}
