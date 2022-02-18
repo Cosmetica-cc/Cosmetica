6f597062ca492f02989a48cbebf1d7a5958aee29
Valoeghese <valoeghesemods@gmail.com>
2022-02-18 15:58:11 +1300

    render wack cape formats nicely

--- src/main/java/com/eyezah/cosmetics/SessionWrapperService.java
+++ src/main/java/com/eyezah/cosmetics/SessionWrapperService.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.eyezah.cosmetics.mixin.textures.MixinNativeImageAccessor;
 import com.eyezah.cosmetics.utils.Debug;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.exceptions.AuthenticationException;
@@ -12,6 +13,7 @@ import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.blaze3d.platform.NativeImage;
+import org.lwjgl.system.MemoryUtil;
 
 public class SessionWrapperService implements MinecraftSessionService {
 	public SessionWrapperService(MinecraftSessionService original) {
@@ -52,27 +54,103 @@ public class SessionWrapperService implements MinecraftSessionService {
 		int srcWidth = image.getWidth();
 		int srcHeight = image.getHeight();
 
-		if (srcHeight == srcWidth) {
-			// imagine using square textures in 2022
-			return image;
-		} else if (22 * srcWidth == 46 * srcHeight) {
+		if (srcWidth != srcHeight * 2) {
 			// very bad format from a very bad server
 			int height = 32;
+			int scaleFactor = 1;
 
 			// get the right dimensions
 			while (height < srcHeight) {
 				height *= 2;
+				scaleFactor *= 2;
 			}
 
+			// copy and dump dest
 			NativeImage result = new NativeImage(height * 2, height, true);
-			result.copyFrom(image);
+			copyImage(image, result);
 
+			// ancient cape format
+			if (17 * srcWidth == 22 * srcHeight) {
+				// time to make an elytra
+				copyRect(result, 1, 1, 35, 4, 11, 16, scaleFactor);
+				// on right
+				clearRect(result, 44, 4, 2, 3, scaleFactor);
+				clearRect(result, 45, 7, 1, 4, scaleFactor);
+				// on left
+				clearRect(result, 35, 16, 2, 4, scaleFactor);
+				clearRect(result, 35, 11, 1, 5, scaleFactor);
+				clearRect(result, 37, 19, 1, 1, scaleFactor);
+				// expand below
+				copyRect(result, 38, 19, 38, 20, 8, 1, scaleFactor);
+				copyRect(result, 39, 19, 39, 21, 7, 1, scaleFactor);
+				// expand above
+				copyRect(result, 35, 4, 35, 3, 8, 1, scaleFactor);
+				copyRect(result, 35, 4, 35, 2, 7, 1, scaleFactor);
+				// the top bit thing
+				copyRect(result, 12, 0, 31, 0 , 9, 1, scaleFactor);
+				copyRect(result, 32, 0, 32, 1 , 2, 1, scaleFactor);
+				copyRect(result, 35, 2, 34, 2, 2, 2, scaleFactor);
+				// the edge bit
+				copyRect(result, 45, 11, 22, 11 , 1, 11, scaleFactor);
+			}
+
+			// debug stuff
 			String imgid = "cape_" + System.currentTimeMillis();
 			Debug.dumpImages(imgid + "_src", true, image);
-			Debug.dumpImages(imgid + "_formatted", true, image);
+			Debug.dumpImages(imgid + "_formatted", true, result);
+
+			image.close();
 			return result;
 		} else {
 			return image;
+		}
+	}
+
+	// fixed version of mojank code
+	private static void copyImage(NativeImage src, NativeImage dest) {
+		int width = Math.min(dest.getWidth(), src.getWidth());
+		int height = Math.min(dest.getHeight(), src.getHeight());
+		int bytesPerPixel = src.format().components();
+
+		for(int l = 0; l < height; ++l) {
+			int m = l * src.getWidth() * bytesPerPixel;
+			int n = l * dest.getWidth() * bytesPerPixel;
+			MemoryUtil.memCopy(
+					((MixinNativeImageAccessor) (Object) src).getPixels() + (long)m,
+					((MixinNativeImageAccessor) (Object) dest).getPixels() + (long)n,
+					width * bytesPerPixel);
+		}
+	}
+
+	private static void copyRect(NativeImage img, int srcX, int srcY, int destX, int destY, int width, int height, int scale) {
+		srcX *= scale;
+		srcY *= scale;
+
+		destX *= scale;
+		destY *= scale;
+
+		width *= scale;
+		height *= scale;
+
+		for(int dx = 0; dx < width; ++dx) {
+			for(int dy = 0; dy < height; ++dy) {
+				int colour = img.getPixelRGBA(srcX + dx, srcY + dy);
+				img.setPixelRGBA(destX + dx, destY + dy, colour);
+			}
+		}
+	}
+
+	private static void clearRect(NativeImage img, int x, int y, int width, int height, int scale) {
+		x *= scale;
+		y *= scale;
+
+		width *= scale;
+		height *= scale;
+
+		for(int dx = 0; dx < width; ++dx) {
+			for(int dy = 0; dy < height; ++dy) {
+				img.setPixelRGBA(x + dx, y + dy, 0);
+			}
 		}
 	}
 }
