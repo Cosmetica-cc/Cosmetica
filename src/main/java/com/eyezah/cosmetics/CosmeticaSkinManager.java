8dc5d5615442c3d384a61d7beed815f6e2f552ef
Valoeghese <valoeghesemods@gmail.com>
2022-02-22 18:32:42 +1300

    further progress on multiplayer skin service thing

--- src/main/java/com/eyezah/cosmetics/CosmeticaSkinManager.java
+++ src/main/java/com/eyezah/cosmetics/CosmeticaSkinManager.java
@@ -1,24 +1,80 @@
 package com.eyezah.cosmetics;
 
 import com.eyezah.cosmetics.mixin.textures.MixinNativeImageAccessor;
+import com.eyezah.cosmetics.mixin.textures.MixinPlayerUpdateAccessor;
+import com.eyezah.cosmetics.mixin.textures.MixinYggdrasilAuthenticationServiceInvoker;
 import com.eyezah.cosmetics.utils.Debug;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.properties.Property;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
 import com.mojang.authlib.yggdrasil.response.MinecraftProfilePropertiesResponse;
 import com.mojang.blaze3d.platform.NativeImage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
+import org.apache.commons.codec.binary.Base64;
 import org.lwjgl.system.MemoryUtil;
 
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CosmeticaSkinManager {
+	public static void modifyServerGameProfiles(List<ClientboundPlayerInfoPacket.PlayerUpdate> entries, MixinYggdrasilAuthenticationServiceInvoker yggi) {
+		for (ClientboundPlayerInfoPacket.PlayerUpdate data : entries) {
+			//Cosmetica.runOffthread(() -> {
+				GameProfile existing = data.getProfile();
+				URL url = CosmeticaSkinManager.getCosmeticaURL(null, existing, false);
+
+				if (url != null) {
+					try {
+						CosmeticaSkinManager.CosmeticaProfilePropertiesResponse cmaResponse = yggi.invokeMakeRequest(url, null, CosmeticaSkinManager.CosmeticaProfilePropertiesResponse.class);
+
+						// A ton of nonsense to not use our textures if the server is replacing the skin.
+						// starting with this code to get the original skin
+						var textures = existing.getProperties().get("textures");
+
+						for (Property property : textures) {
+							if (property.getName().equals("textures")) {
+								String decoded = new String(Base64.decodeBase64(property.getValue()), StandardCharsets.UTF_8).trim();
+								System.out.println(decoded);
+
+								JsonObject jo = JsonParser.parseString(decoded).getAsJsonObject();
+								String originalSkinURL = jo.get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString();
+
+								// and finally checking it against our "original skin" requirement
+								if (cmaResponse.getOriginalSkin().equals(originalSkinURL)) {
+									//Minecraft.getInstance().tell(() -> {
+										// now we can replace with our game profile.
+										GameProfile cosmeticaProfile = new GameProfile(cmaResponse.getId(), cmaResponse.getName());
+										cosmeticaProfile.getProperties().putAll(cmaResponse.getProperties());
+										// what
+										existing.getProperties().putAll(cmaResponse.getProperties());
+										// set our one
+										((MixinPlayerUpdateAccessor) data).setProfile(cosmeticaProfile);
+									//});
+								}
+
+								break;
+							}
+						}
+					} catch (Exception e) {
+						Cosmetica.LOGGER.error("Error adding cosmetica skin service to multiplayer skins", e);
+					}
+				}
+			//});
+		}
+	}
+
 	public static class CosmeticaProfilePropertiesResponse extends MinecraftProfilePropertiesResponse {
 		private String originalSkin;
 
--- src/main/java/com/eyezah/cosmetics/mixin/textures/MixinPlayerUpdateAccessor.java
+++ src/main/java/com/eyezah/cosmetics/mixin/textures/MixinPlayerUpdateAccessor.java
@@ -3,10 +3,12 @@ package com.eyezah.cosmetics.mixin.textures;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ClientboundPlayerInfoPacket.PlayerUpdate.class)
 public interface MixinPlayerUpdateAccessor {
 	@Accessor
+	@Mutable
 	void setProfile(GameProfile newProfile);
 }
