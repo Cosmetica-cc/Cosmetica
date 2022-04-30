ba7153b6a276b532113392600b29867c8316267f
Mekal Covic <46117728+valoeghese@users.noreply.github.com>
2022-05-01 09:07:54 +1200

    cleaner

--- src/main/java/com/eyezah/cosmetics/mixin/MixinClientLevel.java
+++ src/main/java/com/eyezah/cosmetics/mixin/MixinClientLevel.java
@@ -27,8 +27,8 @@ import java.util.function.Supplier;
 public abstract class MixinClientLevel extends Level {
 	@Shadow @Final private Minecraft minecraft;
 
-	protected MixinClientLevel(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, DimensionType dimensionType, Supplier<ProfilerFiller> supplier, boolean bl, boolean bl2, long l) {
-		super(writableLevelData, resourceKey, (Holder<DimensionType>) dimensionType, supplier, bl, bl2, l);
+	protected MixinClientLevel(WritableLevelData writableLevelData, ResourceKey<Level> resourceKey, Holder<DimensionType> dimensionType, Supplier<ProfilerFiller> supplier, boolean bl, boolean bl2, long l) {
+		super(writableLevelData, resourceKey, dimensionType, supplier, bl, bl2, l);
 	}
 
 	@Inject(at = @At("RETURN"), method = "tick")
