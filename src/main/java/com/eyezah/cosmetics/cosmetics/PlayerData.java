a4a78b10311795e64bef58784e191758f71ee803
Valoeghese <valoeghesemods@gmail.com>
2022-06-20 10:43:18 +1200

    fix multihat rendering

--- src/main/java/com/eyezah/cosmetics/cosmetics/PlayerData.java
+++ src/main/java/com/eyezah/cosmetics/cosmetics/PlayerData.java
@@ -2,13 +2,15 @@ package com.eyezah.cosmetics.cosmetics;
 
 import com.eyezah.cosmetics.cosmetics.model.BakableModel;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Data class for Player Data
  */
-public record PlayerData(String lore, boolean upsideDown, String prefix, String suffix, List<BakableModel> hats, BakableModel leftShoulderBuddy, BakableModel rightShoulderBuddy, BakableModel backBling, ResourceLocation cape) {
+public record PlayerData(String lore, boolean upsideDown, String prefix, String suffix, List<BakableModel> hats, @Nullable BakableModel leftShoulderBuddy, @Nullable BakableModel rightShoulderBuddy, @Nullable BakableModel backBling, @Nullable ResourceLocation cape) {
 
-	public static PlayerData NONE = new PlayerData("", false, "", "", null, null, null, null, null);
+	public static PlayerData NONE = new PlayerData("", false, "", "", new ArrayList<>(), null, null, null, null);
 }
--- src/main/java/com/eyezah/cosmetics/mixin/screen/MixinConnectScreenInvoker.java
+++ src/main/java/com/eyezah/cosmetics/mixin/screen/MixinConnectScreenInvoker.java
@@ -1,17 +1,10 @@
 package com.eyezah.cosmetics.mixin.screen;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ConnectScreen;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.multiplayer.resolver.ServerAddress;
 import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(ConnectScreen.class)
 public interface MixinConnectScreenInvoker {
