3556d8cb3068e391f934b4313733127050df216c
Valoeghese <valoeghesemods@gmail.com>
2022-06-28 10:59:05 +1200

    fix #51

--- src/main/java/com/eyezah/cosmetics/mixin/textures/ElytraLayerMixin.java
+++ src/main/java/com/eyezah/cosmetics/mixin/textures/ElytraLayerMixin.java
@@ -1,5 +1,8 @@
 package com.eyezah.cosmetics.mixin.textures;
 
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.VertexMultiConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.layers.ElytraLayer;
 import net.minecraft.resources.ResourceLocation;
@@ -13,7 +16,15 @@ public abstract class ElytraLayerMixin {
 			method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;armorCutoutNoCull(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/renderer/RenderType;")
 	)
-	private RenderType allowTransparentWings(ResourceLocation location) {
-		return RenderType.entityTranslucent(location);
+	private RenderType enableElytraTransparency(ResourceLocation resourceLocation) {
+		return RenderType.entityTranslucent(resourceLocation);
+	}
+
+	@Redirect(
+			method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/world/entity/LivingEntity;FFFFFF)V",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;getArmorFoilBuffer(Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/renderer/RenderType;ZZ)Lcom/mojang/blaze3d/vertex/VertexConsumer;")
+	)
+	private VertexConsumer allowTransparentWings(MultiBufferSource buffers, RenderType layer, boolean armour, boolean glint) {
+		return glint ? VertexMultiConsumer.create(buffers.getBuffer(RenderType.entityGlint()), buffers.getBuffer(layer)) : buffers.getBuffer(layer);
 	}
 }
