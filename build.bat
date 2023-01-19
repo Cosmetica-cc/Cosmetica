a13a8e560a646381c6b06ad52a4033aa521f41ec
Valoeghese <valoeghesemods@gmail.com>
2023-01-19 18:52:43 +1300

    hecking windows

--- build.bat
+++ build.bat
@@ -1,15 +1,15 @@
 git checkout master
-.\gradlew build
+call .\gradlew build
 git checkout 1.19
-.\gradlew build
+call .\gradlew build
 git checkout 1.19.1
-.\gradlew build
+call .\gradlew build
 git checkout 1.19.3
-.\gradlew build
+call .\gradlew build
 git checkout 1.18.1
-.\gradlew build
+call .\gradlew build
 git checkout 1.17
-.\gradlew build
+call .\gradlew build
 git checkout 1.16.5
-.\gradlew build
+call .\gradlew build
 git checkout master
