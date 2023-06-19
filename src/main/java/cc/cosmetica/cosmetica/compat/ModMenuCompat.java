2caebdd5fcce016c904d24ebaceecda1326d6e54
Valoeghese <valoeghesemods@gmail.com>
2023-06-20 11:28:48 +1200

    readd modmenu support

--- src/main/java/cc/cosmetica/cosmetica/compat/ModMenuCompat.java
+++ src/main/java/cc/cosmetica/cosmetica/compat/ModMenuCompat.java
@@ -17,16 +17,13 @@
 package cc.cosmetica.cosmetica.compat;
 
 import cc.cosmetica.cosmetica.screens.LoadingScreen;
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.minecraft.client.Minecraft;
 
-// TODO uncomment this when mod menu is released on 1.20
-// Perhaps just include mod menu as compile and not in runtime for now.
-
-public class ModMenuCompat {//implements ModMenuApi {
-//	@Override
-//	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//		return parent -> new LoadingScreen(parent, Minecraft.getInstance().options);
-//	}
+public class ModMenuCompat implements ModMenuApi {
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> new LoadingScreen(parent, Minecraft.getInstance().options);
+	}
 }
