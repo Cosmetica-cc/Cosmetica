21c0b3dd546d2aa1341ab6a190b1f1486445b7a1
Valoeghese <valoeghesemods@gmail.com>
2023-10-23 23:13:45 +1300

    config refactor and new armour config option

--- src/main/java/cc/cosmetica/cosmetica/config/CosmeticaConfig.java
+++ src/main/java/cc/cosmetica/cosmetica/config/CosmeticaConfig.java
@@ -23,21 +23,37 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.function.Function;
 
 public class CosmeticaConfig {
-    private final Path propertiesPath;
-    private boolean showNametagInThirdPerson = true;
-    private boolean showNonVitalUpdateMessages = true;
-    private WelcomeMessageState showWelcomeMessage = WelcomeMessageState.FULL;
-    private boolean addCosmeticaSplashMessage = true;
-    private boolean regionalEffectsPrompt = true;
-    private boolean paranoidHttps = false;
-
     public CosmeticaConfig(Path propertiesPath) {
         this.propertiesPath = propertiesPath;
     }
+
+    private final Path propertiesPath;
+    private final List<Option<?>> options = new ArrayList<>();
+
+    private final Option<Boolean> showNametagInThirdPerson = new Option<>("show-nametag-in-third-person", true, Boolean::parseBoolean);
+    private final Option<Boolean> hideNonVitalUpdateMessages = new Option<>("honestly-believe-me-i-know-what-the-heck-i-am-doing-trust-me-bro-ask-joe-if-you-dont-believe-me", false, Boolean::parseBoolean);
+    private final Option<WelcomeMessageState> showWelcomeMessage = new Option<>(
+            "show-welcome-message",
+            WelcomeMessageState.FULL,
+            WelcomeMessageState::of,
+            s -> s.toString().toLowerCase(Locale.ROOT)
+    );
+    private final Option<Boolean> addCosmeticaSplashMessage = new Option<>("add-cosmetica-splash-message", true, Boolean::parseBoolean);
+    private final Option<Boolean> regionalEffectsPrompt = new Option<>("regional-effects-prompt", true, Boolean::parseBoolean);
+    private final Option<Boolean> paranoidHttps = new Option<>("paranoid-https", false, Boolean::parseBoolean);
+    private final Option<ArmourConflictHandlingMode> armourConflictHandlingMode = new Option<>(
+            "armour-conflict-handling-mode",
+            ArmourConflictHandlingMode.HIDE_COSMETICS,
+            s -> ArmourConflictHandlingMode.valueOf(s.toUpperCase(Locale.ROOT)),
+            mode -> mode.toString().toLowerCase(Locale.ROOT)
+    );
 
     public void initialize() throws IOException {
         load();
@@ -51,24 +67,22 @@ public class CosmeticaConfig {
         }
 
         Properties properties = new Properties();
-        properties.setProperty("show-nametag-in-third-person", "true"); // default
-        properties.setProperty("show-welcome-message", "full"); // default 2: electric boogaloo
-        properties.setProperty("honestly-believe-me-i-know-what-the-heck-i-am-doing-trust-me-bro-ask-joe-if-you-dont-believe-me", "false"); // default 3: the return of the king
-        properties.setProperty("add-cosmetica-splash-message", "true");
-        properties.setProperty("regional-effects-prompt", "true");
-        properties.setProperty("paranoid-https", "false");
 
+        // add defaults
+        for (Option<?> option : this.options) {
+            properties.setProperty(option.getName(), option.getSerialisedValue());
+        }
+
+        // load properties
         properties.load(Files.newInputStream(propertiesPath));
-        showNametagInThirdPerson    =  Boolean.parseBoolean(properties.getProperty("show-nametag-in-third-person"));
-        showWelcomeMessage =  WelcomeMessageState.of(properties.getProperty("show-welcome-message"));
-        showNonVitalUpdateMessages  = !Boolean.parseBoolean(properties.getProperty("honestly-believe-me-i-know-what-the-heck-i-am-doing-trust-me-bro-ask-joe-if-you-dont-believe-me"));
-        addCosmeticaSplashMessage   =  Boolean.parseBoolean(properties.getProperty("add-cosmetica-splash-message"));
-        regionalEffectsPrompt       =  Boolean.parseBoolean(properties.getProperty("regional-effects-prompt"));
-        paranoidHttps               =  Boolean.parseBoolean(properties.getProperty("paranoid-https"));
+
+        for (Option<?> option : this.options) {
+            option.loadValue(properties.getProperty(option.getName()));
+        }
 
         // update paranoid https status on existing API instance, if one exists
         if (Cosmetica.api != null) {
-            Cosmetica.api.setForceHttps(paranoidHttps);
+            Cosmetica.api.setForceHttps(paranoidHttps.getValue());
         }
     }
 
@@ -77,41 +91,107 @@ public class CosmeticaConfig {
         if (!parentDir.exists()) parentDir.mkdir();
 
         Properties properties = new Properties();
-        properties.setProperty("show-nametag-in-third-person", String.valueOf(showNametagInThirdPerson));
-        properties.setProperty("show-welcome-message", String.valueOf(showWelcomeMessage).toLowerCase(Locale.ROOT));
-        properties.setProperty("honestly-believe-me-i-know-what-the-heck-i-am-doing-trust-me-bro-ask-joe-if-you-dont-believe-me", String.valueOf(!showNonVitalUpdateMessages));
-        properties.setProperty("add-cosmetica-splash-message", String.valueOf(addCosmeticaSplashMessage));
-        properties.setProperty("regional-effects-prompt", String.valueOf(regionalEffectsPrompt));
-        properties.setProperty("paranoid-https", String.valueOf(paranoidHttps));
+
+        for (Option<?> option : options) {
+            properties.setProperty(option.getName(), option.getSerialisedValue());
+        }
+
         properties.store(Files.newOutputStream(propertiesPath), "Cosmetica Config");
     }
 
     public boolean shouldShowNametagInThirdPerson() {
-        return showNametagInThirdPerson;
+        return showNametagInThirdPerson.getValue();
     }
 
     public void setShowNametagInThirdPerson(boolean showNametagInThirdPerson) {
-        this.showNametagInThirdPerson = showNametagInThirdPerson;
+        this.showNametagInThirdPerson.setValue(showNametagInThirdPerson);
     }
 
     public WelcomeMessageState showWelcomeMessage() {
-        return showWelcomeMessage;
+        return showWelcomeMessage.getValue();
     }
 
     public boolean shouldShowNonVitalUpdateMessages() {
-        return showNonVitalUpdateMessages;
+        return !hideNonVitalUpdateMessages.getValue();
     }
 
     public boolean shouldAddCosmeticaSplashMessage() {
-        return addCosmeticaSplashMessage;
+        return addCosmeticaSplashMessage.getValue();
     }
 
     public boolean regionalEffectsPrompt() {
-        return regionalEffectsPrompt;
+        return regionalEffectsPrompt.getValue();
     }
 
     public boolean paranoidHttps() {
-        return paranoidHttps;
+        return paranoidHttps.getValue();
+    }
+
+    public ArmourConflictHandlingMode getArmourConflictHandlingMode() {
+        return this.armourConflictHandlingMode.getValue();
+    }
+
+    public void setArmourConflictHandlingMode(ArmourConflictHandlingMode mode) {
+        this.armourConflictHandlingMode.setValue(mode);
+    }
+
+    private class Option<T> {
+        Option(String name, T defaultValue, Function<String, T> deserialiser) {
+            this(name, defaultValue, deserialiser, String::valueOf);
+        }
+
+        Option(String name, T defaultValue, Function<String, T> deserialiser, Function<T, String> serialiser) {
+            this.name = name;
+            this.value = defaultValue;
+            this.deserialiser = deserialiser;
+            this.serialiser = serialiser;
+            CosmeticaConfig.this.options.add(this);
+        }
+
+        private final String name;
+        private final Function<String, T> deserialiser;
+        private final Function<T, String> serialiser;
+        private T value;
+
+        public String getName() {
+            return this.name;
+        }
+
+        public T getValue() {
+            return this.value;
+        }
+
+        public String getSerialisedValue() {
+            return this.serialiser.apply(this.value);
+        }
+
+        public void loadValue(String serialisedForm) {
+            this.value = this.deserialiser.apply(serialisedForm);
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * The set of modes for handling server-declared conflicts between cosmetics and armour.
+     * @apiNote This will almost certainly be replaced by a better solution we have in the works for Cosmetica 2.0.
+     * In the meantime, this is easier to implement and should satisfy everyone.
+     */
+    public enum ArmourConflictHandlingMode {
+        /**
+         * The default solution, and the solution used prior to this version.
+         */
+        HIDE_COSMETICS,
+        /**
+         * Hide the armour instead of the cosmetics.
+         */
+        HIDE_ARMOUR,
+        /**
+         * Show both the cosmetic and armour, as if the flag invoking this behaviour was never set.
+         */
+        SHOW_BOTH
     }
 
     public enum WelcomeMessageState {
