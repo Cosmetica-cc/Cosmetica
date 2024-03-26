9f562dd886d52be6573fd65d058006bd4e45ca5f
Valoeghese <valoeghesemods@gmail.com>
2024-03-26 17:08:41 +1300

    move mixin to a more reliable location

---
+++ src/main/java/cc/cosmetica/cosmetica/mixin/ClientboundPlayerInfoUpdatePacketActionMixin.java
@@ -0,0 +1,25 @@
+package cc.cosmetica.cosmetica.mixin;
+
+import cc.cosmetica.cosmetica.Cosmetica;
+import cc.cosmetica.cosmetica.ThreadPool;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientboundPlayerInfoUpdatePacket.Action.class)
+public class ClientboundPlayerInfoUpdatePacketActionMixin {
+	// This captures only other players
+	// See comment in Cosmetica.forwardPublicUserInfoToNametag
+	@Inject(at = @At("RETURN"), method = "method_46342")
+	private static void afterAddPlayer(ClientboundPlayerInfoUpdatePacket.EntryBuilder entryBuilder, FriendlyByteBuf friendlyByteBuf, CallbackInfo ci) {
+		final GameProfile profile = entryBuilder.profile;
+
+		if (profile != null) {
+			Cosmetica.runOffthread(() -> Cosmetica.forwardPublicUserInfoToNametag(profile), ThreadPool.GENERAL_THREADS);
+		}
+	}
+}
